eb196f04e86b9bc8eb7d7125a78848744e24afec
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2023-09-29 09:50:37 -0700

    updated

--- script.js
+++ script.js
@@ -9,32 +9,32 @@ tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
 btn.addEventListener('click', navToggle)
 
 function onTabClick(e) {
-    tabs.forEach((tab) => {
-        tab.children[0].classList.remove(
-        'border-softRed',
-        'border-b-4',
-        'md:border-b-0'
-        )
-    })
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove(
+      'border-softRed',
+      'border-b-4',
+      'md:border-b-0'
+    )
+  })
 
-    panels.forEach((panel) => panel.classList.add('hidden'))
+  panels.forEach((panel) => panel.classList.add('hidden'))
 
-    e.target.classList.add('border-softRed', 'border-b-4')
-    const classString = e.target.getAttribute('data-target')
-    document
-        .querySelector('#panels')
-        .getElementsByClassName(classString)[0]
-        .classList.remove('hidden')
-    }
+  e.target.classList.add('border-softRed', 'border-b-4')
+  const classString = e.target.getAttribute('data-target')
+  document
+    .querySelector('#panels')
+    .getElementsByClassName(classString)[0]
+    .classList.remove('hidden')
+}
 
-    function navToggle() {
-    btn.classList.toggle('open')
-    menu.classList.toggle('flex')
-    menu.classList.toggle('hidden')
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
 
-    if (menu.classList.contains('flex')) {
-        logo.setAttribute('src', './images/logo-bookmark-footer.svg')
-    } else {
-        logo.setAttribute('src', './images/logo-bookmark.svg')
-    }
+  if (menu.classList.contains('flex')) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg')
+  }
 }
